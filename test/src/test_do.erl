51c1407de9323b65029bb19e43c418d3a1ee51ae
Kozlov Yakov <yk@exante.eu>
2015-01-30 10:47:48 +0300

    Simplify do transformations logic. Fixed maps support

--- test/src/test_do.erl
+++ test/src/test_do.erl
@@ -42,25 +42,6 @@ maybe(Arg) ->
         || monad_plus:guard(maybe_m, is_number(Arg)),
            return(Arg*Arg)]).
 
-test_fib() ->
-    true = lists:all(fun ({X, Y}) -> X =:= Y end,
-                     [{fib_m(N), fib_rec(N)} || N <- lists:seq(0, 20)]).
-
-%% Classic monadic implementation of fibonnaci
-fib_m(N) ->
-    StateT = state_t:new(identity_m),
-    {_, R} = StateT:exec(
-               monad:sequence(StateT,
-                              lists:duplicate(N, fib_m_step(StateT))), {0, 1}),
-    R.
-
-fib_m_step(StateT) -> StateT:modify(fun ({X, Y}) -> {Y, X+Y} end).
-
-%% Classic recursive implementation of fibonnaci
-fib_rec(N) when N >= 0 -> fib_rec(N, 0, 1).
-fib_rec(0, _X, Y) -> Y;
-fib_rec(N,  X, Y) -> fib_rec(N-1, Y, X+Y).
-
 test_list() ->
     %% Demonstrate equivalence of list comprehensions and list monad
     A = [{X,Y} || X <- "abcd",
@@ -90,27 +71,6 @@ test_omega() ->
                        return({X,Y,Z})]),
     true = A =/= B,
     true = A =:= lists:usort(B).
-
-test_error_t_list() ->
-    M = error_t:new(list_m),
-    R = M:run(do([M || E1 <- M:lift([1, 2, 3]),
-                       E2 <- M:lift([4, 5, 6]),
-                       case (E1 * E2) rem 2 of
-                           0 -> return({E1, E2});
-                           _ -> fail(not_even_product)
-                       end])),
-    R = [{ok, {1, 4}}, {error, not_even_product}, {ok, {1, 6}},
-         {ok, {2, 4}}, {ok, {2, 5}},              {ok, {2, 6}},
-         {ok, {3, 4}}, {error, not_even_product}, {ok, {3, 6}}],
-
-    %% Compare with the non-error_t version, which will remove failures:
-    S = do([list_m || E1 <- [1, 2, 3],
-                      E2 <- [4, 5, 6],
-                      case (E1 * E2) rem 2 of
-                          0 -> return({E1, E2});
-                          _ -> fail(not_even_product)
-                      end]),
-    S = [{1, 4}, {1, 6}, {2, 4}, {2, 5}, {2, 6}, {3, 4}, {3, 6}].
 
 %% Tests for 'let-match binding' (a-la 'let' in Haskell's 'do'
 %% expression) But instead of 'let' here we use 'match' (=) expression
@@ -191,10 +151,8 @@ test() ->
     test:test([{?MODULE, [test_sequence,
                           test_join,
                           test_maybe,
-                          test_fib,
                           test_list,
                           test_omega,
-                          test_error_t_list,
                           test_let_match,
                           test_let_first,
                           test_let_escapes]}],
